0a7f00893b0a18688a602cded255bb7bb1ea0def
kishore <kishore@leaguex.com>
2025-05-11 16:31:58 +0530

    feat: added native android bridge module

--- README.md
+++ README.md
@@ -1,12 +1,53 @@
+![NPM Version](https://img.shields.io/npm/v/@thealteroffice/react-native-adgeist)
+
+---
+
 # @thealteroffice/react-native-adgeist
 
-Publishers can integrate our SDK to connect their ad spaces to the AdGeist marketplace
+A React Native library that allows publishers can integrate our SDK to connect their ad spaces to the AdGeist marketplace.
+Written in Kotlin, Swift and Typescript. It supports both the Old and New React Native architecture.
 
 ## Installation
+
+### React Native Bare
+
+You can install the package via npm or yarn:
 
 ```sh
 npm install @thealteroffice/react-native-adgeist
 ```
+
+```sh
+yarn add @thealteroffice/react-native-adgeist
+```
+
+Don't forget to run pod-install.
+
+### Expo
+
+You can install the package like any other Expo package, using the following command:
+
+```sh
+npx expo install @thealteroffice/react-native-adgeist
+```
+
+## Setup
+
+### Expo
+
+Simply add the library plugin to your `app.json` file:
+
+```json
+{
+  "expo": {
+    "plugins": ["@thealteroffice/react-native-adgeist"]
+  }
+}
+```
+
+This way, Expo will handle the native setup for you during `prebuild`.
+
+> Note: only SDK 50 and above are supported, the plugin is configured to handle only the kotlin template.
 
 ## Usage
 
--- android/src/main/java/com/adgeist/AdgeistModule.kt
+++ android/src/main/java/com/adgeist/AdgeistModule.kt
@@ -2,22 +2,88 @@ package com.adgeist
 
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.annotations.ReactModule
+import com.facebook.react.turbomodule.core.interfaces.TurboModule
+import com.facebook.react.bridge.Promise
+import android.util.Log
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.WritableMap
+import com.adgeistkit.AdgeistCore
+import com.adgeistkit.CreativeDataModel
+import com.adgeistkit.Campaign
+import com.adgeistkit.Creative
+import com.adgeistkit.BudgetSettings
 
 @ReactModule(name = AdgeistModule.NAME)
 class AdgeistModule(reactContext: ReactApplicationContext) :
-  NativeAdgeistSpec(reactContext) {
+  NativeAdgeistSpec(reactContext), TurboModule {
+
+  private val adgeistInstanceFromLibrary = AdgeistCore.initialize(reactContext.applicationContext)
+  private val getAd = adgeistInstanceFromLibrary.getCreative()
+  private val postCreativeAnalytic = adgeistInstanceFromLibrary.postCreativeAnalytics()
+
 
   override fun getName(): String {
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
-  override fun multiply(a: Double, b: Double): Double {
-    return a * b
+  override fun fetchCreative(adSpaceId: String, publisherId: String, promise: Promise) {
+    getAd.fetchCreative(adSpaceId, publisherId) { adData ->
+      if (adData != null) {
+        promise.resolve(adData.toWritableMap())
+      } else {
+        promise.reject("NO_AD", "Ad data not available")
+      }
+    }
+  }
+
+  override fun sendCreativeAnalytic(campaignId: String, adSpaceId: String, publisherId: String, eventType: String, promise: Promise) {
+    postCreativeAnalytic.sendTrackingData(campaignId, adSpaceId, publisherId, eventType) { adData ->
+      if (adData != null) {
+        Log.d("MyActivity of app module", adData)
+        promise.resolve(adData)
+      } else {
+        promise.reject("NO_AD", "Couldn't find the campaign to update analytics")
+      }
+    }
   }
 
   companion object {
     const val NAME = "Adgeist"
   }
+}
+
+// Extension function to convert CreativeData to WritableMap
+fun CreativeDataModel.toWritableMap(): WritableMap {
+  val map = Arguments.createMap()
+  map.putBoolean("success", success)
+  map.putString("message", message)
+
+  val dataMap = Arguments.createMap()
+  data?.let { campaign ->
+    dataMap.putString("_id", campaign._id)
+    dataMap.putString("name", campaign.name)
+
+    val creativeMap = Arguments.createMap()
+    campaign.creative?.let { creative ->
+      creativeMap.putString("title", creative.title)
+      creativeMap.putString("description", creative.description)
+      creativeMap.putString("fileUrl", creative.fileUrl)
+      creativeMap.putString("ctaUrl", creative.ctaUrl)
+      creativeMap.putString("type", creative.type)
+      creativeMap.putString("fileName", creative.fileName)
+      creativeMap.putString("createdAt", creative.createdAt)
+      creativeMap.putString("updatedAt", creative.updatedAt)
+    }
+    dataMap.putMap("creative", creativeMap)
+
+    val budgetMap = Arguments.createMap()
+    campaign.budgetSettings?.let { budget ->
+      budgetMap.putDouble("totalBudget", budget.totalBudget)
+      budgetMap.putDouble("spentBudget", budget.spentBudget)
+    }
+    dataMap.putMap("budgetSettings", budgetMap)
+  }
+
+  map.putMap("data", dataMap)
+  return map
 }
