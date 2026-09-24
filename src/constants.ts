6bd448a4007fbeb1f80f3f699379a26e2df133ee
Kishore <Kishore@thealteroffice.com>
2026-09-24 12:21:16 +0530

    chore: version update

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const PACKAGE_VERSION_TAG = 'RN';
-export const PACKAGE_VERSION = '0.0.34';
+export const PACKAGE_VERSION = '0.0.35';
 
 export const HTML_5_AD_NATIVE_COMPONENT_NAME = 'HTML5AdNativeComponent';
